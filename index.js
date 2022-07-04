51fbc2530e806ae92331b0f3699c64d8d2f7591b
DeepthiT-131 <77687005+DeepthiT-131@users.noreply.github.com>
2022-07-04 15:08:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,52 @@ const nodemailer = require("nodemailer");
 exports.handler = async (event) => {
     // TODO implement
    const data = event;
-  
+   let info ;
    const transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
-      user: 'vpcreators1@gmail.com', //sender emailId
+      user: 'vpcreators1@gmail.com',
       pass: 'cqaq evyr fatt pmfh',
     },
   });
-  let info = await transport
+
+
+if(!data.email){
+  info = await transport
     .sendMail({
-      from: `${data.FirstName} <${data.Email}>`,
-      to: "praveen@antstack.io",                    //receiver emailId
-      subject: `Enquiry from ${data.FirstName} ${data.LastName}!! `,
+      from: `${data.firstName} <${data.email?data.email: " "}>`,
+      to: "vpcreators1@gmail.com",
+      subject: `Enquiry from ${data.firstName}!! `,
+    
       html: `
-      <p> Hello VP creators , you have received an enquiry from ${data.FirstName} ${data.LastName} saying <strong>"${data.Message}"</strong></p>
+      <p> Hello VP creators , you have received an enquiry from ${data.firstName?data.firstName: " "} </p>
       <h3>Details</h3>
-      <p style="margin:0"><span>Name<span> : <strong>${data.FirstName} ${data.LastName}<strong/></p>
-      <p style="margin:0"><span>Phone Number<span> : <strong>${data.PhoneNumber}<strong/></p>
-      <p style="margin:0"><span>E-Mail<span> <strong>${data.Email}<strong/></p>
-      
+      <p style="margin:0"><span>Name<span> : <strong>${data.firstName?data.firstName:" "} ${data.lastName?data.lastName:" "}<strong/></p>
+      <p style="margin:0"><span>Phone Number<span> : <strong>${data.phoneNumber?data.phoneNumber:" "}<strong/></p>
+      <p style="margin:0"><span>Message<span>: <strong>${data.message?data.message: " "}<strong/></p>
       `
-    })
-    .then((res) => {
-      console.log(res);
+});
+}
+else{
+info = await transport
+    .sendMail({
+      from: `${data.firstName} <${data.email?data.email:" "}>`,
+      to: "vpcreators1@gmail.com",
+      subject: `Enquiry from ${data.firstName}!! `,
+      html: `
+      <p> Hello VP creators , you have received an enquiry from ${data.firstName} </p>
+      <h3>Details</h3>
+      <p style="margin:0"><span>Name<span> : <strong>${data.firstName?data.firstName:"  "} ${data.lastName?data.lastName:" "}<strong/></p>
+      <p style="margin:0"><span>Phone Number<span> : <strong>${data.phoneNumber?data.phoneNumber:" "}<strong/></p>
+      <p style="margin:0"><span>E-Mail<span> :<strong>${data.email?data.email:" "}<strong/></p>
+      <p style="margin:0"><span>Message<span>: <strong>${data.message?data.message: " "}<strong/></p>
+      `
     });
-  console.log('message sent', info);
-  return info;
+}
+    console.log('message sent', info);
+  return info ;
 };
 
 
